8f4b65a3e78b6d908685045c3b467a5152194866
maruthisenthil <senthilbdpm@gmail.com>
2026-02-04 11:53:27 +0530

    My first commit for Python practice Jan 2026

---
+++ print_variable_examples.py
@@ -0,0 +1,20 @@
+#Python   Practice Exercises
+#1: Print "Hello, World!" to the console.
+print("Hello, Senthil!");
+#2: Create a variable called 'name' and assign it your name. Then print "Hello, [name]!".
+name = "Senthil"
+print("Hello, " + name + "!");
+
+employee_id = 12345
+print("Employee ID:", employee_id)
+employee_name ="Senthil Kumar"
+print("Employee Name:", employee_name)
+employee_age = 30
+print("Employee Age:", employee_age)
+employee_salary = 75000.50
+print("Employee Salary:", employee_salary)
+employee_isActive  = True
+employee_
+#3 Print the data type of the variable 'name'.
+print(type(name));
+
