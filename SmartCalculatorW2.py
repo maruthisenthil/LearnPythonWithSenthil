c2ee0c8b3284e64ef9701024d079b1d628036651
maruthisenthil <senthilbdpm@gmail.com>
2026-02-08 17:36:10 +0530

    Week2 - 3rd session

---
+++ SmartCalculatorW2.py
@@ -0,0 +1,15 @@
+num1 = float(input('Enter the first number: '))
+num2 = float(input('Enter the second number: '))
+
+#perform calculations
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1/num2 
+
+print("<==== Calculation Results ====>")
+print(f"Addition of {num1} and {num2}", addition)
+print(f"SUbtration of {num1} and {num2}", subtraction)
+print(f"Division of {num1} and {num2}", division)
+print(f"Multiplication of {num1} and {num2}", multiplication)
---
+++ week2.py
@@ -0,0 +1,9 @@
+# name ="Karthik"
+print(name)
+# name = 'Karhik'
+print(name)
+name ='''KARTHIK'''
+print(name)
+
+salary = 900000
+print(f"Employee {name} earns  {salary} per month ")
